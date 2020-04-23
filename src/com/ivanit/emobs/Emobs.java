991e1d7429fa90bb0d3c3e6d48eba3460c65dad2
Michael I <mivanit@hotmail.com>
2020-04-23 12:36:02 -0400

    setting up java plugin

---
+++ src/com/ivanit/emobs/Emobs.java
@@ -0,0 +1,15 @@
+package com.ivanit.emobs;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Emobs extends JavaPlugin {
+	//	Fired when plugin is enabled
+	@Override
+    public void onEnable() {
+		
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+}
